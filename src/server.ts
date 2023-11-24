8c3f179537bea8cd29d289f055bfdbbef8734495
Asem-Abdelhady <asemshawkey@gmail.com>
2023-11-25 01:10:32 +0300

    Add: express server with socket.io

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import http from "http";
+import { Server as SocketIoServer } from "socket.io";
+import cors from "cors";
+const app = express();
+const server = http.createServer(app);
+const io = new SocketIoServer(server);
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", (message: string) => {
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
