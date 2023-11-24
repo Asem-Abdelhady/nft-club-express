d05489ad421974ef558298338b8d727ecf7f66f8
Asem-Abdelhady <asemshawkey@gmail.com>
2023-11-25 01:35:48 +0300

    Add: configuration

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,16 @@ import { Server as SocketIoServer } from "socket.io";
 import cors from "cors";
 const app = express();
 const server = http.createServer(app);
-const io = new SocketIoServer(server);
 
+// Allow requests from any origin
 app.use(cors());
+
+const io = new SocketIoServer(server, {
+  cors: {
+    origin: "*", // Allow any origin
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("A user connected");
